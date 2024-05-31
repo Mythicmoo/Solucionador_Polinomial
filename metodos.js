62aa496c85976b9a09d781677907772be3b77f3e
Breno Barcellos <bpbarcellos2112@gmail.com>
2024-05-31 18:22:08 -0300

    0.3
    entrada de dados da função métodos, implementada

--- metodos.js
+++ metodos.js
@@ -1,14 +1,30 @@
 const math = require('mathjs')
 
-function f(x) {
-    return x**3 - 9*x + 3
+function ResolverPolinomio() {
+
+// pega os dados da interface e organiza em operações polinomiais
+function f(x=1) {
+    // a*x**5 + b*x**4 + c*x**3 + d*x**2 + e*x + f = f(x)
+    let a = parseFloat(document.getElementById('a').value)
+    let b = parseFloat(document.getElementById('b').value)
+    let c = parseFloat(document.getElementById('c').value)
+    let d = parseFloat(document.getElementById('d').value)
+    let e = parseFloat(document.getElementById('e').value)
+    let f = parseFloat(document.getElementById('f').value)
+    return a*x**5 + b*x**4 + c*x**3 + d*x**2 + e*x + f
 }
 
-function df(x) {
-    return 3*x**2 - 9
+function df(x=1) {
+    let a = parseFloat(document.getElementById('a').value)
+    let b = parseFloat(document.getElementById('b').value)
+    let c = parseFloat(document.getElementById('c').value)
+    let d = parseFloat(document.getElementById('d').value)
+    let e = parseFloat(document.getElementById('e').value)
+    return (5*a)*x**4 + (4*b)*x**3 + (3*c)*x**2 + (2*d)*x + e
 }
 
-var x0 = 1.5; var eps1 = 0.0001; var eps2  = eps1; var maxiter = 100; var xk = 0; var lista = []; var iter = 0;
+
+/*var x0 = 1.5; var eps1 = 0.0001; var eps2  = eps1; var maxiter = 100; var xk = 0; var lista = []; var iter = 0;
 
 while ( math.abs(f(x0)) > eps1 && iter <= maxiter) {
     
@@ -25,4 +41,8 @@ while ( math.abs(f(x0)) > eps1 && iter <= maxiter) {
     iter = iter + 1;
 }
 
-console.log(lista, iter)
+console.log(lista, iter)*/
+
+var p = document.getElementById("dados");
+    p.innerText = p.innerText +  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus debitis hic fugit est officiis aliquam neque natus consequuntur deserunt minima assumenda perferendis eveniet dolorum, quasi numquam laboriosam ullam deleniti. Aperiam?\n";
+}
--- teste.js
+++ teste.js
@@ -1,4 +1,25 @@
-function teste() {
-    var p = document.getElementById("dados");
-    p.innerText = p.innerText +  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus debitis hic fugit est officiis aliquam neque natus consequuntur deserunt minima assumenda perferendis eveniet dolorum, quasi numquam laboriosam ullam deleniti. Aperiam?\n";
+const math = require('mathjs')
+
+function f(x) {
+    // a*x**5 + b*x**4 + c*x**3 + d*x**2 + e*x + f = f(x)
+    let a = 2; //parseFloat(document.getElementById('a').value)
+    let b = 4; //parseFloat(document.getElementById('b').value)
+    let c = -3; //parseFloat(document.getElementById('c').value)
+    let d = -1; //parseFloat(document.getElementById('d').value)
+    let e = 5; //parseFloat(document.getElementById('e').value)
+    let f = 1; //parseFloat(document.getElementById('f').value)
+    // retorna [operação , string]
+    return a*x**5 + b*x**4 + c*x**3 + d*x**2 + e*x + f
 }
+
+function df(x) {
+    let a = 2; //parseFloat(document.getElementById('a').value)
+    let b = 4; //parseFloat(document.getElementById('b').value)
+    let c = -3; //parseFloat(document.getElementById('c').value)
+    let d = -1; //parseFloat(document.getElementById('d').value)
+    let e = 5; //parseFloat(document.getElementById('e').value)
+    // retorna [operação , string]
+    return (5*a)*x**4 + (4*b)*x**3 + (3*c)*x**2 + (2*d)*x + e
+}
+
+console.log(f(2), df(2))
