9ab3d8b4510bda6f0fb54e935ffa3181216c7e1b
Breno Barcellos <bpbarcellos2112@gmail.com>
2024-05-31 16:49:39 -0300

    0.2
    função de iteração implementada

--- metodos.js
+++ metodos.js
@@ -1,7 +1,5 @@
 const math = require('mathjs')
 
-
-
 function f(x) {
     return x**3 - 9*x + 3
 }
@@ -10,16 +8,21 @@ function df(x) {
     return 3*x**2 - 9
 }
 
+var x0 = 1.5; var eps1 = 0.0001; var eps2  = eps1; var maxiter = 100; var xk = 0; var lista = []; var iter = 0;
 
-const x = math.range(5.0, 20.0 , 0.1).toArray();
-console.log(x)
+while ( math.abs(f(x0)) > eps1 && iter <= maxiter) {
+    
+    xk = x0 - f(x0)/df(x0);
 
-
-var x0 = 1.5; var eps1 = 0.1; var eps2  = eps1; var maxiter = 20;
-var lista = []
-var iter = 0
-
-while (math.abs(f(x0))>eps1 && iter<=maxiter) {
-    console.log(`[${x0}]`)
-
+    lista.push(xk)
+    
+    if(math.abs(f(x0)) < eps1 || math.abs(xk - x0) < eps2){
+        break;
+    }else{
+        x0 = xk
+    };
+    
+    iter = iter + 1;
 }
+
+console.log(lista, iter)
